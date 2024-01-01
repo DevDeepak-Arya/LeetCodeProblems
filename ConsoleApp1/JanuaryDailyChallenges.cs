bdcbf1a5b4e227e541fe65dcec3f16608ae028c6
DevDeepak-Arya <aryadeepak087.da@gmail.com>
2024-01-01 11:17:00 +0530

    Jan1/dailyChallenge - Assign Cookies

---
+++ ConsoleApp1/JanuaryDailyChallenges.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public static class JanuaryDailyChallenges
+    {
+        public enum Approach
+        {
+            Brute,
+            Better,
+            Optimal
+        }
+
+        #region 455. Assign Cookies - Easy
+        // Explaination https://www.youtube.com/watch?v=2oZD3GfboFo
+        public static int FindContentChildren(int[] g, int[] s)
+        {
+            Array.Sort(g);
+            Array.Reverse(g);
+            Array.Sort(s);
+            Array.Reverse(s);
+
+
+            int satisfiedChilds = 0;
+            int cookiePointer = 0;
+
+            for (int i = 0; i < g.Length; i++)
+            {
+
+                if (cookiePointer < s.Length && g[i] <= s[cookiePointer])
+                {
+                    satisfiedChilds++;
+                    cookiePointer++;
+                }
+            }
+            return satisfiedChilds;
+
+        }
+
+        #endregion
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,8 +3,10 @@ using LeetCodeProblems;
 
 int[] ar= { 3, 0, 1 };
 string[] str = { "ab", "a" };
+int[] g={10, 9, 8, 7};
+int[] s = { 5, 6, 7, 8 };
 
-Console.WriteLine(Arrays.MissingNumber(ar,Arrays.Approach.Optimal));
+Console.WriteLine(JanuaryDailyChallenges.FindContentChildren(g,s));
 Console.ReadLine();
 
 
